e38351375b39ad7b59c388ea981bee39a6b7d87d
Srdjan Stjepanovic <businessrdjan@gmail.com>
2022-04-24 18:35:18 +0200

    Added equipment windows

---
+++ Sims-Hospital-Zdravo/View/Manager/ManagerEquipment.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Controller;
+
+namespace Sims_Hospital_Zdravo.View.Manager
+{
+    /// <summary>
+    /// Interaction logic for ManagerEquipment.xaml
+    /// </summary>
+    public partial class ManagerEquipment : Window
+    {
+
+        private EquipmentController equipmentController;
+        private EquipmentTransferController equipmentTransferController;
+        private RoomController roomController;
+        public ManagerEquipment(EquipmentController equipmentController, EquipmentTransferController equipmentTransferController, RoomController roomController)
+        {
+            this.equipmentController = equipmentController;
+            this.equipmentTransferController = equipmentTransferController;
+            this.roomController = roomController;
+            InitializeComponent();
+            equipmentTable.ItemsSource = equipmentController.ReadAll();
+        }
+
+        private void InsertRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Transfer_Click(object sender, RoutedEventArgs e)
+        {
+ 
+                EquipmentTransfer equipmentTransfer = new EquipmentTransfer(equipmentController, equipmentTransferController, roomController);
+                equipmentTransfer.Show();
+
+
+        }
+    }
+}
