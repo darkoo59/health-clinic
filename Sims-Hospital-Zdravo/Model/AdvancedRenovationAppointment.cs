3812f37f5b9cb897dbf2aa2b9c50a2a35c4e15e8
Srdjan Stjepanovic <businessrdjan@gmail.com>
2022-05-06 19:58:26 +0200

    Added model for advanced renovation

---
+++ Sims-Hospital-Zdravo/Model/AdvancedRenovationAppointment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Model;
+
+public enum RoomRenovationType
+{
+    SPLIT,
+    JOIN
+}
+
+namespace Sims_Hospital_Zdravo.Model
+{
+    public class AdvancedRenovationAppointment : RenovationAppointment
+    {
+        public List<Room> rooms { get; }
+        public RoomRenovationType roomRenovationType { get; set; }
+
+        public AdvancedRenovationAppointment(TimeInterval time, Room room, string description, List<Room> rooms, RoomRenovationType roomRenovationType, int id) : base(time, room, description, RenovationType.ADVANCED, id)
+        {
+            this.rooms = rooms;
+            this.roomRenovationType = roomRenovationType;
+        }
+    }
+}
