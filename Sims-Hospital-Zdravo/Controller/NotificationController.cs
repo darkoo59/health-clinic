6434c0007704709c28f734f545cb5c0ef1c3008f
Srdjan Stjepanovic <businessrdjan@gmail.com>
2022-05-11 20:53:41 +0200

    Added new files for notifications

---
+++ Sims-Hospital-Zdravo/Controller/NotificationController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Sims_Hospital_Zdravo.Model;
+using Sims_Hospital_Zdravo.Repository;
+using Sims_Hospital_Zdravo.Service;
+
+namespace Sims_Hospital_Zdravo.Controller
+{
+    public class NotificationController
+    {
+        private NotificationService _notificationService;
+
+        public NotificationController(NotificationService notificationService)
+        {
+            this._notificationService = notificationService;
+        }
+
+        public List<Notification> ReadAll()
+        {
+            return _notificationService.ReadAll();
+        }
+
+        public Notification FindById(int id)
+        {
+            return _notificationService.FindById(id);
+        }
+
+        public void Delete(Notification notification)
+        {
+            _notificationService.Delete(notification);
+        }
+
+        public void DeleteById(int id)
+        {
+            _notificationService.DeleteById(id);
+        }
+
+        public void Create(Notification notification)
+        {
+            _notificationService.Create(notification);
+        }
+
+        public List<Notification> ReadAllManagerMedicineNotifications()
+        {
+            return _notificationService.ReadAllManagerMedicineNotifications();
+        }
+
+        public int GenerateId()
+        {
+            return _notificationService.GenerateId();
+        }
+    }
+}
