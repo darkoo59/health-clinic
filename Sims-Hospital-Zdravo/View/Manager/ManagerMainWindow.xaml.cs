2040c3a9e6d745d1c69ba246dbed4e506da26220
Srdjan Stjepanovic <businessrdjan@gmail.com>
2022-05-16 19:15:22 +0200

    Observer removal on window closing

--- Sims-Hospital-Zdravo/View/Manager/ManagerMainWindow.xaml.cs
+++ Sims-Hospital-Zdravo/View/Manager/ManagerMainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
@@ -110,6 +111,11 @@ namespace Sims_Hospital_Zdravo.View.Manager
             }
 
             return "Medicine " + notification._Medicine._Name + " has beeen declined!";
+        }
+
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            app._taskScheduleTimer.RemoveObserver(this);
         }
     }
 }
