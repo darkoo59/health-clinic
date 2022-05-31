0a5b50849147f5f9bc8b72d99b52674819b5133a
darkoo59 <darko.selakovic11@gmail.com>
2022-05-31 15:25:15 +0200

    Changes in data files

--- Sims-Hospital-Zdravo/PatientUpdate.xaml.cs
+++ Sims-Hospital-Zdravo/PatientUpdate.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
--- Sims-Hospital-Zdravo/Utils/TaskScheduleTimer.cs
+++ Sims-Hospital-Zdravo/Utils/TaskScheduleTimer.cs
@@ -68,7 +68,7 @@ namespace Sims_Hospital_Zdravo.Utils
             CheckIfThereShouldBeNotification();
             CheckNotificationForManager();
             CheckNotificationForDoctor();
-            AppointmentDone();
+            //AppointmentDone();
         }
 
         private void CheckIfRelocationAppointmentDone()
