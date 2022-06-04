2a50f13981345b78a8a9801a477ad73e9f7616af
darkoo59 <darko.selakovic11@gmail.com>
2022-06-04 14:37:42 +0200

    Added scheduling meetings GUI

---
+++ Sims-Hospital-Zdravo/Model/Meeting.cs
@@ -0,0 +1,33 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sims_Hospital_Zdravo.Model
+{
+    public class Meeting
+    {
+        private DateTime _start;
+        private Room _room;
+        private List<User> _optionalAttendees;
+        private List<User> _requiredAttendees;
+
+        public Meeting(DateTime start, Room room, List<User> optionalAttendees, List<User> requiredAttendees)
+        {
+            _start = start;
+            _room = room;
+            _optionalAttendees = optionalAttendees;
+            _requiredAttendees = requiredAttendees;
+        }
+
+        public DateTime Start { get; set; }
+
+        public Room Room { get; set; }
+
+        public List<User> OptionalAttendees { get; set; }
+
+        public List<User> RequiredAttendees { get; set; }
+    }
+}
